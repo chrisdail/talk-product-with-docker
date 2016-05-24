44d5179912d8ccb94c801b99822424984b23aeef
Chris Dail <Christopher.Dail@emc.com>
2016-05-24 09:43:39 -0300

    Added setup of registry

--- demo/run.sh
+++ demo/run.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -v $(pwd)/installer/playbooks:/playbooks -v $(pwd):/playbooks/data -w /playbooks chrisdail/ansible:stable ansible-playbook -i /playbooks/data/inventory $@
+docker run --rm -it -v $(pwd)/installer/playbooks:/playbooks -v $(pwd)/build:/playbooks/data -w /playbooks chrisdail/ansible:stable ansible-playbook -i /playbooks/data/inventory $@
